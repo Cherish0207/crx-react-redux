165779545061450eb6495eaaf22ff8ac0ae34280
Cherish0207 <cheshire_1207@foxmail.com>
2021-06-15 10:24:47 +0800

    6.中间件的核心：重写dispatch方法

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,24 @@
 import { createStore } from "../redux";
 import rootReducer from "./reducers";
 const store = createStore(rootReducer);
+
+/**
+ * 实现一个日志中间件・状态改变前和改变后打印一些日志
+ * --- 切片 
+ * 中间件的核心：重写dispatch方法
+ */
+let dispatch = store.dispatch;
+store.dispatch = function (action) {
+  console.log("prev state", store.getState());
+  dispatch(action);
+  console.log("next state", store.getState());
+  return action;
+};
+
+store.dispatch = function (action) {
+  setTimeout(() => {
+    dispatch(action);
+  }, 3000);
+  return action;
+};
 export default store;
